d6aa5241e77a8965975953dc02a74f275b517cde
tpoechtrager <t.poechtrager@gmail.com>
2023-06-18 19:32:46 +0200

    Bump version

--- zte.js
+++ zte.js
@@ -7,7 +7,7 @@
  * 
  */
 
-console.log("Loading ZTE Script v" + "2023-06-XX-#1");
+console.log("Loading ZTE Script v" + "2023-06-18-#1");
 
 siginfo =
     "wan_active_band,wan_active_channel,wan_lte_ca,wan_apn,wan_ipaddr," +
