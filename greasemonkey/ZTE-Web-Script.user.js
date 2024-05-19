9ef48da690981ff1c6088c245e9aeebb7c25ca31
Thomas Pöchtrager <tpoechtrager@users.noreply.github.com>
2024-05-19 13:45:58 +0200

    Update ZTE-Web-Script.user.js
    Make the script work with Tampermonkey and Greasemonkey,
    also add more match URLs

--- greasemonkey/ZTE-Web-Script.user.js
+++ greasemonkey/ZTE-Web-Script.user.js
@@ -1,14 +1,57 @@
 // ==UserScript==
 // @name         ZTE-Web-Script
 // @version      0.1
-// @description  Automatically loading ZTE-Web-Script 
-// @include      http://192.168.1.1/*
-// @include      http://192.168.2.1/*
-// @require https://code.jquery.com/jquery-3.7.1.min.js
+// @description  Automatically loading ZTE-Web-Script
+// @match        https://192.168.0.1/*
+// @match        https://192.168.1.1/*
+// @match        https://192.168.2.1/*
+// @match        http://192.168.0.1/*
+// @match        http://192.168.1.1/*
+// @match        http://192.168.2.1/*
+// @require      https://code.jquery.com/jquery-3.7.1.min.js
+// @grant        GM_xmlhttpRequest
 // ==/UserScript==
-//--- The @grant directive is used to restore the proper sandbox.
 
-var ztes = document.createElement("script");
-ztes.type = "text/javascript";
-ztes.src = "https://cdn.jsdelivr.net/gh/tpoechtrager/ZTE-Web-Script/zte.js";
-$("head").append(ztes);
+(function() {
+    'use strict';
+
+    function loadScript(url, onload, onerror) {
+        if (typeof GM_xmlhttpRequest !== "undefined") {
+            GM_xmlhttpRequest({
+                method: "GET",
+                url: url,
+                onload: function(response) {
+                    var script = document.createElement('script');
+                    script.textContent = response.responseText;
+                    document.head.appendChild(script);
+                    if (onload) onload();
+                },
+                onerror: onerror
+            });
+        } else {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", url, true);
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        var script = document.createElement('script');
+                        script.textContent = xhr.responseText;
+                        document.head.appendChild(script);
+                        if (onload) onload();
+                    } else {
+                        if (onerror) onerror();
+                    }
+                }
+            };
+            xhr.onerror = onerror;
+            xhr.send();
+        }
+    }
+
+    loadScript("https://cdn.jsdelivr.net/gh/tpoechtrager/ZTE-Web-Script/zte.js", function() {
+        console.log("ZTE-Web-Script loaded and executed.");
+    }, function() {
+        alert("Failed to load the ZTE-Web-Script");
+    });
+
+})();
